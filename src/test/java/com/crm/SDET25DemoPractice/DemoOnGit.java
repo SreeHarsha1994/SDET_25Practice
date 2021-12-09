9d93dd5582d52988aef32c886bbd095f00a77dbf
SREEHARSH <SREEHARSH@LAPTOP-SO7U3JNL>
2021-12-09 13:38:11 +0530

    My first commit and Push.

---
+++ src/test/java/com/crm/SDET25DemoPractice/DemoOnGit.java
@@ -0,0 +1,11 @@
+package com.crm.SDET25DemoPractice;
+
+import org.testng.annotations.Test;
+
+public class DemoOnGit {
+	@Test
+	public void gitDemo() {
+		System.out.println("My 1st commit and push");
+	}
+
+}
